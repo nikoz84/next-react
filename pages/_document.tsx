845ea4ff9de32c9dabeb2e1d7c84aa92b9582ec3
nikoz84 <nicolas.hernandez@dasa.com.br>
2023-03-13 14:05:12 -0300

    add config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+            type="text/css"
+          />
+      </Head>
+      <body style={{ fontFamily: "Comfortaa, sans-serif" }}>
         <Main />
         <NextScript />
       </body>
