0b4224d379b961705d28785520c3334312a63d37
nikoz84 <nicolas.hernandez@dasa.com.br>
2023-03-21 15:50:52 -0300

    change about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,7 +1,16 @@
+import Title from "@/src/components/Title"
+import Link from "next/link"
+
 export default function AboutPage(){
     return (
-    <>
-        <h1>SOBRE</h1>
-    </>
+        <>
+            <h1>ABOUT PAGE</h1>
+            <Title asTag="h2">
+                Título
+            </Title>
+            <Link href="/">
+                Go to home page
+            </Link>
+        </>
     )
 }
