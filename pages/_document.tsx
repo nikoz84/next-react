a051061829ee0eda88dc7bacc43051ff29b923b6
nikoz84 <nicolas.hernandez@dasa.com.br>
2023-03-23 11:48:43 -0300

    add husky

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,13 +17,13 @@ export default function Document() {
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `
-            window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-NQN7NBL045');
+                        window.dataLayer = window.dataLayer || [];
+                          function gtag(){dataLayer.push(arguments);}
+                          gtag('js', new Date());
+                        
+                          gtag('config', 'G-NQN7NBL045');
 
-          `,
+                      `,
                     }}
                 ></script>
             </Head>
