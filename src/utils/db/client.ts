d7596ed1da6647f02765ed4461aae2366206ae81
Nicolás Romero <nikoz.1984@gmail.com>
2023-04-19 00:40:46 -0300

    fix: new changes to fix problems with server pages

--- src/utils/db/client.ts
+++ src/utils/db/client.ts
@@ -4,15 +4,15 @@ declare global {
     var prisma: PrismaClient
 }
 
-let prisma: PrismaClient;
+let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+    prisma = new PrismaClient()
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+    if (!global.prisma) {
+        global.prisma = new PrismaClient()
+    }
+    prisma = global.prisma
 }
 
-export default prisma;
+export default prisma
